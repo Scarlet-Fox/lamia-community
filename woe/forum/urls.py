9cf7e2af3fd67f1597126ae4e6dd641b739b3e72
Scarly Crow <spuriousscarlet@gmail.com>
2015-04-22 19:13:11 -0700

    Added signing out

--- woe/forum/urls.py
+++ woe/forum/urls.py
@@ -3,5 +3,6 @@ from forum import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^sign-in/$', views.SignInView.as_view(), name='signin'),
+    url(r'^sign-in/$', views.SignInView.as_view()),
+    url(r'^sign-out/$', views.sign_out),
 ]
--- woe/forum/views.py
+++ woe/forum/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
-# Create your views here.
 def index(request):
     context = {}
     return render(request, "general/base.jade", context)
-    
+
+@csrf_exempt
+def sign_out(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("/")
+
 class SignInView(FormView):
     template_name = "general/sign_in.jade"
     form_class = forms.SignInForm
@@ -16,3 +22,4 @@ class SignInView(FormView):
     def form_valid(self, form):
         login(self.request, form.cleaned_data["authenticated_user"])
         return super(SignInView, self).form_valid(form)
+        
