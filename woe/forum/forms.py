445366f6fa5d3a73a7d81cbc3b98debacdee2388
Scarly Crow <spuriousscarlet@gmail.com>
2015-04-23 19:05:34 -0700

    Working registration, tested by Zoop. Fixed sign in bugs.

--- woe/forum/forms.py
+++ woe/forum/forms.py
@@ -4,7 +4,7 @@ import arrow
 
 class SignInForm(forms.Form):
     username = forms.CharField(label='Username', max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(label='Password', max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Password', max_length=255, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label='Email address', max_length=255, widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
     
     def clean(self):
--- woe/forum/views.py
+++ woe/forum/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from . import models
 from . import forms
 
 def index(request):
@@ -29,4 +31,20 @@ class RegisterView(FormView):
     success_url = "/"
     
     def form_valid(self, form):
-        print "okay"
+        cleaned_data = form.cleaned_data
+        u = User.objects.create_user(
+            cleaned_data["username"].lower().strip(), 
+            cleaned_data["email"], 
+            cleaned_data["password"].strip()
+        )
+        u.save()
+        p = models.Profile(
+            user = u,
+            how_found = cleaned_data["how_did_you_find_us"],
+            display_name = cleaned_data["username"].strip()
+        )
+        p.save()
+        # SETUP EMAIL ADDRESS STUFF
+        return super(RegisterView, self).form_valid(form)
+        
+        
