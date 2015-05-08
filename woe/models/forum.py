3bb7027114302e719e84a3d08333e0ec38adc2dc
Scarly Crow <spuriousscarlet@gmail.com>
2015-05-08 16:01:07 -0700

    Added a full topic model

--- woe/models/forum.py
+++ woe/models/forum.py
@@ -24,13 +24,37 @@ class Post(db.Document):
     flag_score = db.IntField(default=0)
     flag_clear_date = db.DateTimeField()
     flags = db.EmbeddedDocumentField(Flag)
-    
+
+class Prefix(db.Document):
+    pre_html = db.StringField()
+    post_html = db.StringField()
+    prefix = db.StringField()
+
 class Topic(db.Document):
     # Basics
+    category = db.ReferenceField("Category")
     title = db.StringField()
     creator = db.ReferenceField(core.User)
-    created = db.DateTimeField
+    created = db.DateTimeField()
+    
+    sticky = db.BooleanField()
+    hidden = db.BooleanField()
+    closed = db.BooleanField()
+    announcement = db.BooleanField()
+    
+    # Prefixes
+    pre_html = db.StringField()
+    post_html = db.StringField()
+    prefix = db.StringField()
+    
+    # Background info
+    watchers = db.ListField(db.ReferenceField(core.User))
+    topic_moderators = db.ListField(db.ReferenceField(core.User))
+    user_post_counts = db.DictField()
+    data = db.DictField()
     
     # Tracking
-    post_count = db.IntField
-    
+    post_count = db.IntField()
+    last_post_by = db.ReferenceField(core.User)
+    last_post_date = db.DateTimeField()
+    last_post_author_avatar = db.StringField()
