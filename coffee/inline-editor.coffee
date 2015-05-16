44bdfe54a3e367e6d3b7786518abbe2ab025e0f2
Scarly Crow <spuriousscarlet@gmail.com>
2015-05-16 10:19:12 -0700

    Editor is now badass

--- coffee/inline-editor.coffee
+++ coffee/inline-editor.coffee
@@ -10,13 +10,13 @@ $ ->
       if url != ""
         $.get url, (data) =>
           @element.data("editor_initial_html", data.content)
-          do @setupEditor
+          @setupEditor fullpage_option
       else
         @element.data("editor_initial_html", @element.html())
-        do @setupEditor
+        @setupEditor fullpage_option
         
     
-    setupEditor: () =>
+    setupEditor: (fullpage_option=false) =>
       @element.html(@editordivHTML())
       
       @element.before @toolbarHTML
@@ -28,6 +28,7 @@ $ ->
         modules:
           'link-tooltip': true
           'toolbar': { container: "#toolbar-#{@quillID}" }
+        theme: 'snow'
       
       @element.data("editor", quill)
       
@@ -86,22 +87,124 @@ $ ->
     
     toolbarHTML: () =>
       return """
-        <div class="btn-toolbar" role="toolbar" id="toolbar-#{@quillID}">
-          <div class="btn-group" role="group">
-            <button type="button" class="btn btn-default ql-bold"><b>b</b></button>
-            <button type="button" class="btn btn-default ql-italic"><i>i</i></button>
-            <button type="button" class="btn btn-default ql-underline"><u>u</u></button>
-            <button type="button" class="btn btn-default ql-strike"><s>s</s></button>
-          </div>
-          <div class="btn-group" role="group">
+        <div id="toolbar-#{@quillID}" class="toolbar">
+          <span class="ql-format-group">
+            <select title="Font" class="ql-font">
+              <option value="sans-serif" selected>Sans Serif</option>
+              <option value="serif">Serif</option>
+              <option value="monospace">Monospace</option>
+            </select>
             <select title="Size" class="ql-size">
+              <option value="8px">Micro</option>
               <option value="10px">Small</option>
-              <option value="13px" selected="">Normal</option>
+              <option value="14px" selected>Normal</option>
               <option value="18px">Large</option>
               <option value="32px">Huge</option>
             </select>
-            <button type="button" class="btn btn-default ql-link"><span class="glyphicon glyphicon-link"</span></button>
-          </div>
+          </span>
+          <span class="ql-format-group">
+            <span title="Bold" class="ql-format-button ql-bold"></span>
+            <span class="ql-format-separator"></span>
+            <span title="Italic" class="ql-format-button ql-italic"></span>
+            <span class="ql-format-separator"></span>
+            <span title="Underline" class="ql-format-button ql-underline"></span>
+            <span class="ql-format-separator"></span>
+            <span title="Strikethrough" class="ql-format-button ql-strike"></span>
+          </span>
+          <span class="ql-format-group">
+            <select title="Text Color" class="ql-color">
+              <option value="rgb(0, 0, 0)" label="rgb(0, 0, 0)" selected></option>
+              <option value="rgb(230, 0, 0)" label="rgb(230, 0, 0)"></option>
+              <option value="rgb(255, 153, 0)" label="rgb(255, 153, 0)"></option>
+              <option value="rgb(255, 255, 0)" label="rgb(255, 255, 0)"></option>
+              <option value="rgb(0, 138, 0)" label="rgb(0, 138, 0)"></option>
+              <option value="rgb(0, 102, 204)" label="rgb(0, 102, 204)"></option>
+              <option value="rgb(153, 51, 255)" label="rgb(153, 51, 255)"></option>
+              <option value="rgb(255, 255, 255)" label="rgb(255, 255, 255)"></option>
+              <option value="rgb(250, 204, 204)" label="rgb(250, 204, 204)"></option>
+              <option value="rgb(255, 235, 204)" label="rgb(255, 235, 204)"></option>
+              <option value="rgb(255, 255, 204)" label="rgb(255, 255, 204)"></option>
+              <option value="rgb(204, 232, 204)" label="rgb(204, 232, 204)"></option>
+              <option value="rgb(204, 224, 245)" label="rgb(204, 224, 245)"></option>
+              <option value="rgb(235, 214, 255)" label="rgb(235, 214, 255)"></option>
+              <option value="rgb(187, 187, 187)" label="rgb(187, 187, 187)"></option>
+              <option value="rgb(240, 102, 102)" label="rgb(240, 102, 102)"></option>
+              <option value="rgb(255, 194, 102)" label="rgb(255, 194, 102)"></option>
+              <option value="rgb(255, 255, 102)" label="rgb(255, 255, 102)"></option>
+              <option value="rgb(102, 185, 102)" label="rgb(102, 185, 102)"></option>
+              <option value="rgb(102, 163, 224)" label="rgb(102, 163, 224)"></option>
+              <option value="rgb(194, 133, 255)" label="rgb(194, 133, 255)"></option>
+              <option value="rgb(136, 136, 136)" label="rgb(136, 136, 136)"></option>
+              <option value="rgb(161, 0, 0)" label="rgb(161, 0, 0)"></option>
+              <option value="rgb(178, 107, 0)" label="rgb(178, 107, 0)"></option>
+              <option value="rgb(178, 178, 0)" label="rgb(178, 178, 0)"></option>
+              <option value="rgb(0, 97, 0)" label="rgb(0, 97, 0)"></option>
+              <option value="rgb(0, 71, 178)" label="rgb(0, 71, 178)"></option>
+              <option value="rgb(107, 36, 178)" label="rgb(107, 36, 178)"></option>
+              <option value="rgb(68, 68, 68)" label="rgb(68, 68, 68)"></option>
+              <option value="rgb(92, 0, 0)" label="rgb(92, 0, 0)"></option>
+              <option value="rgb(102, 61, 0)" label="rgb(102, 61, 0)"></option>
+              <option value="rgb(102, 102, 0)" label="rgb(102, 102, 0)"></option>
+              <option value="rgb(0, 55, 0)" label="rgb(0, 55, 0)"></option>
+              <option value="rgb(0, 41, 102)" label="rgb(0, 41, 102)"></option>
+              <option value="rgb(61, 20, 102)" label="rgb(61, 20, 102)"></option>
+            </select>
+            <span class="ql-format-separator"></span>
+            <select title="Background Color" class="ql-background">
+              <option value="rgb(0, 0, 0)" label="rgb(0, 0, 0)"></option>
+              <option value="rgb(230, 0, 0)" label="rgb(230, 0, 0)"></option>
+              <option value="rgb(255, 153, 0)" label="rgb(255, 153, 0)"></option>
+              <option value="rgb(255, 255, 0)" label="rgb(255, 255, 0)"></option>
+              <option value="rgb(0, 138, 0)" label="rgb(0, 138, 0)"></option>
+              <option value="rgb(0, 102, 204)" label="rgb(0, 102, 204)"></option>
+              <option value="rgb(153, 51, 255)" label="rgb(153, 51, 255)"></option>
+              <option value="rgb(255, 255, 255)" label="rgb(255, 255, 255)" selected></option>
+              <option value="rgb(250, 204, 204)" label="rgb(250, 204, 204)"></option>
+              <option value="rgb(255, 235, 204)" label="rgb(255, 235, 204)"></option>
+              <option value="rgb(255, 255, 204)" label="rgb(255, 255, 204)"></option>
+              <option value="rgb(204, 232, 204)" label="rgb(204, 232, 204)"></option>
+              <option value="rgb(204, 224, 245)" label="rgb(204, 224, 245)"></option>
+              <option value="rgb(235, 214, 255)" label="rgb(235, 214, 255)"></option>
+              <option value="rgb(187, 187, 187)" label="rgb(187, 187, 187)"></option>
+              <option value="rgb(240, 102, 102)" label="rgb(240, 102, 102)"></option>
+              <option value="rgb(255, 194, 102)" label="rgb(255, 194, 102)"></option>
+              <option value="rgb(255, 255, 102)" label="rgb(255, 255, 102)"></option>
+              <option value="rgb(102, 185, 102)" label="rgb(102, 185, 102)"></option>
+              <option value="rgb(102, 163, 224)" label="rgb(102, 163, 224)"></option>
+              <option value="rgb(194, 133, 255)" label="rgb(194, 133, 255)"></option>
+              <option value="rgb(136, 136, 136)" label="rgb(136, 136, 136)"></option>
+              <option value="rgb(161, 0, 0)" label="rgb(161, 0, 0)"></option>
+              <option value="rgb(178, 107, 0)" label="rgb(178, 107, 0)"></option>
+              <option value="rgb(178, 178, 0)" label="rgb(178, 178, 0)"></option>
+              <option value="rgb(0, 97, 0)" label="rgb(0, 97, 0)"></option>
+              <option value="rgb(0, 71, 178)" label="rgb(0, 71, 178)"></option>
+              <option value="rgb(107, 36, 178)" label="rgb(107, 36, 178)"></option>
+              <option value="rgb(68, 68, 68)" label="rgb(68, 68, 68)"></option>
+              <option value="rgb(92, 0, 0)" label="rgb(92, 0, 0)"></option>
+              <option value="rgb(102, 61, 0)" label="rgb(102, 61, 0)"></option>
+              <option value="rgb(102, 102, 0)" label="rgb(102, 102, 0)"></option>
+              <option value="rgb(0, 55, 0)" label="rgb(0, 55, 0)"></option>
+              <option value="rgb(0, 41, 102)" label="rgb(0, 41, 102)"></option>
+              <option value="rgb(61, 20, 102)" label="rgb(61, 20, 102)"></option>
+            </select>
+          </span>
+          <span class="ql-format-group">
+            <span title="List" class="ql-format-button ql-list"></span>
+            <span class="ql-format-separator"></span>
+            <span title="Bullet" class="ql-format-button ql-bullet"></span>
+            <span class="ql-format-separator"></span>
+            <select title="Text Alignment" class="ql-align">
+              <option value="left" label="Left" selected></option>
+              <option value="center" label="Center"></option>
+              <option value="right" label="Right"></option>
+              <option value="justify" label="Justify"></option>
+            </select>
+          </span>
+          <span class="ql-format-group">
+            <span title="Link" class="ql-format-button ql-link"></span>
+            <span class="ql-format-separator"></span>
+            <span title="Image" class="ql-format-button ql-image"></span>
+          </span>
         </div>
       """
       
--- woe/static/local/js/inline-editor.js
+++ woe/static/local/js/inline-editor.js
@@ -5,21 +5,41 @@
   $(function() {
     var InlineEditor;
     InlineEditor = (function() {
-      function InlineEditor(element, fullpage_option) {
-        var quill;
+      function InlineEditor(element, url, fullpage_option) {
+        if (url == null) {
+          url = "";
+        }
         if (fullpage_option == null) {
           fullpage_option = false;
         }
         this.toolbarHTML = bind(this.toolbarHTML, this);
         this.editordivHTML = bind(this.editordivHTML, this);
         this.submitButtonHTML = bind(this.submitButtonHTML, this);
+        this.setupEditor = bind(this.setupEditor, this);
         this.quillID = this.getQuillID();
         this.element = $(element);
         if (this.element.data("editor_is_active")) {
           return false;
         }
         this.element.data("editor_is_active", true);
-        this.element.data("editor_initial_html", this.element.html());
+        if (url !== "") {
+          $.get(url, (function(_this) {
+            return function(data) {
+              _this.element.data("editor_initial_html", data.content);
+              return _this.setupEditor(fullpage_option);
+            };
+          })(this));
+        } else {
+          this.element.data("editor_initial_html", this.element.html());
+          this.setupEditor(fullpage_option);
+        }
+      }
+
+      InlineEditor.prototype.setupEditor = function(fullpage_option) {
+        var quill;
+        if (fullpage_option == null) {
+          fullpage_option = false;
+        }
         this.element.html(this.editordivHTML());
         this.element.before(this.toolbarHTML);
         this.element.after(this.submitButtonHTML(fullpage_option));
@@ -30,7 +50,8 @@
             'toolbar': {
               container: "#toolbar-" + this.quillID
             }
-          }
+          },
+          theme: 'snow'
         });
         this.element.data("editor", quill);
         $("#save-text-" + this.quillID).click((function(_this) {
@@ -41,7 +62,7 @@
             }
           };
         })(this));
-        $("#cancel-edit-" + this.quillID).click((function(_this) {
+        return $("#cancel-edit-" + this.quillID).click((function(_this) {
           return function(e) {
             e.preventDefault();
             if (_this.cancelFunction != null) {
@@ -49,7 +70,7 @@
             }
           };
         })(this));
-      }
+      };
 
       InlineEditor.prototype.getQuillID = function() {
         return Quill.editors.length + 1;
@@ -94,7 +115,7 @@
       };
 
       InlineEditor.prototype.toolbarHTML = function() {
-        return "<div class=\"btn-toolbar\" role=\"toolbar\" id=\"toolbar-" + this.quillID + "\">\n  <div class=\"btn-group\" role=\"group\">\n    <button type=\"button\" class=\"btn btn-default ql-bold\"><b>b</b></button>\n    <button type=\"button\" class=\"btn btn-default ql-italic\"><i>i</i></button>\n    <button type=\"button\" class=\"btn btn-default ql-underline\"><u>u</u></button>\n    <button type=\"button\" class=\"btn btn-default ql-strike\"><s>s</s></button>\n  </div>\n  <div class=\"btn-group\" role=\"group\">\n    <select title=\"Size\" class=\"ql-size\">\n      <option value=\"10px\">Small</option>\n      <option value=\"13px\" selected=\"\">Normal</option>\n      <option value=\"18px\">Large</option>\n      <option value=\"32px\">Huge</option>\n    </select>\n    <button type=\"button\" class=\"btn btn-default ql-link\"><span class=\"glyphicon glyphicon-link\"</span></button>\n  </div>\n</div>";
+        return "<div id=\"toolbar-" + this.quillID + "\" class=\"toolbar\">\n  <span class=\"ql-format-group\">\n    <select title=\"Font\" class=\"ql-font\">\n      <option value=\"sans-serif\" selected>Sans Serif</option>\n      <option value=\"serif\">Serif</option>\n      <option value=\"monospace\">Monospace</option>\n    </select>\n    <select title=\"Size\" class=\"ql-size\">\n      <option value=\"8px\">Micro</option>\n      <option value=\"10px\">Small</option>\n      <option value=\"14px\" selected>Normal</option>\n      <option value=\"18px\">Large</option>\n      <option value=\"32px\">Huge</option>\n    </select>\n  </span>\n  <span class=\"ql-format-group\">\n    <span title=\"Bold\" class=\"ql-format-button ql-bold\"></span>\n    <span class=\"ql-format-separator\"></span>\n    <span title=\"Italic\" class=\"ql-format-button ql-italic\"></span>\n    <span class=\"ql-format-separator\"></span>\n    <span title=\"Underline\" class=\"ql-format-button ql-underline\"></span>\n    <span class=\"ql-format-separator\"></span>\n    <span title=\"Strikethrough\" class=\"ql-format-button ql-strike\"></span>\n  </span>\n  <span class=\"ql-format-group\">\n    <select title=\"Text Color\" class=\"ql-color\">\n      <option value=\"rgb(0, 0, 0)\" label=\"rgb(0, 0, 0)\" selected></option>\n      <option value=\"rgb(230, 0, 0)\" label=\"rgb(230, 0, 0)\"></option>\n      <option value=\"rgb(255, 153, 0)\" label=\"rgb(255, 153, 0)\"></option>\n      <option value=\"rgb(255, 255, 0)\" label=\"rgb(255, 255, 0)\"></option>\n      <option value=\"rgb(0, 138, 0)\" label=\"rgb(0, 138, 0)\"></option>\n      <option value=\"rgb(0, 102, 204)\" label=\"rgb(0, 102, 204)\"></option>\n      <option value=\"rgb(153, 51, 255)\" label=\"rgb(153, 51, 255)\"></option>\n      <option value=\"rgb(255, 255, 255)\" label=\"rgb(255, 255, 255)\"></option>\n      <option value=\"rgb(250, 204, 204)\" label=\"rgb(250, 204, 204)\"></option>\n      <option value=\"rgb(255, 235, 204)\" label=\"rgb(255, 235, 204)\"></option>\n      <option value=\"rgb(255, 255, 204)\" label=\"rgb(255, 255, 204)\"></option>\n      <option value=\"rgb(204, 232, 204)\" label=\"rgb(204, 232, 204)\"></option>\n      <option value=\"rgb(204, 224, 245)\" label=\"rgb(204, 224, 245)\"></option>\n      <option value=\"rgb(235, 214, 255)\" label=\"rgb(235, 214, 255)\"></option>\n      <option value=\"rgb(187, 187, 187)\" label=\"rgb(187, 187, 187)\"></option>\n      <option value=\"rgb(240, 102, 102)\" label=\"rgb(240, 102, 102)\"></option>\n      <option value=\"rgb(255, 194, 102)\" label=\"rgb(255, 194, 102)\"></option>\n      <option value=\"rgb(255, 255, 102)\" label=\"rgb(255, 255, 102)\"></option>\n      <option value=\"rgb(102, 185, 102)\" label=\"rgb(102, 185, 102)\"></option>\n      <option value=\"rgb(102, 163, 224)\" label=\"rgb(102, 163, 224)\"></option>\n      <option value=\"rgb(194, 133, 255)\" label=\"rgb(194, 133, 255)\"></option>\n      <option value=\"rgb(136, 136, 136)\" label=\"rgb(136, 136, 136)\"></option>\n      <option value=\"rgb(161, 0, 0)\" label=\"rgb(161, 0, 0)\"></option>\n      <option value=\"rgb(178, 107, 0)\" label=\"rgb(178, 107, 0)\"></option>\n      <option value=\"rgb(178, 178, 0)\" label=\"rgb(178, 178, 0)\"></option>\n      <option value=\"rgb(0, 97, 0)\" label=\"rgb(0, 97, 0)\"></option>\n      <option value=\"rgb(0, 71, 178)\" label=\"rgb(0, 71, 178)\"></option>\n      <option value=\"rgb(107, 36, 178)\" label=\"rgb(107, 36, 178)\"></option>\n      <option value=\"rgb(68, 68, 68)\" label=\"rgb(68, 68, 68)\"></option>\n      <option value=\"rgb(92, 0, 0)\" label=\"rgb(92, 0, 0)\"></option>\n      <option value=\"rgb(102, 61, 0)\" label=\"rgb(102, 61, 0)\"></option>\n      <option value=\"rgb(102, 102, 0)\" label=\"rgb(102, 102, 0)\"></option>\n      <option value=\"rgb(0, 55, 0)\" label=\"rgb(0, 55, 0)\"></option>\n      <option value=\"rgb(0, 41, 102)\" label=\"rgb(0, 41, 102)\"></option>\n      <option value=\"rgb(61, 20, 102)\" label=\"rgb(61, 20, 102)\"></option>\n    </select>\n    <span class=\"ql-format-separator\"></span>\n    <select title=\"Background Color\" class=\"ql-background\">\n      <option value=\"rgb(0, 0, 0)\" label=\"rgb(0, 0, 0)\"></option>\n      <option value=\"rgb(230, 0, 0)\" label=\"rgb(230, 0, 0)\"></option>\n      <option value=\"rgb(255, 153, 0)\" label=\"rgb(255, 153, 0)\"></option>\n      <option value=\"rgb(255, 255, 0)\" label=\"rgb(255, 255, 0)\"></option>\n      <option value=\"rgb(0, 138, 0)\" label=\"rgb(0, 138, 0)\"></option>\n      <option value=\"rgb(0, 102, 204)\" label=\"rgb(0, 102, 204)\"></option>\n      <option value=\"rgb(153, 51, 255)\" label=\"rgb(153, 51, 255)\"></option>\n      <option value=\"rgb(255, 255, 255)\" label=\"rgb(255, 255, 255)\" selected></option>\n      <option value=\"rgb(250, 204, 204)\" label=\"rgb(250, 204, 204)\"></option>\n      <option value=\"rgb(255, 235, 204)\" label=\"rgb(255, 235, 204)\"></option>\n      <option value=\"rgb(255, 255, 204)\" label=\"rgb(255, 255, 204)\"></option>\n      <option value=\"rgb(204, 232, 204)\" label=\"rgb(204, 232, 204)\"></option>\n      <option value=\"rgb(204, 224, 245)\" label=\"rgb(204, 224, 245)\"></option>\n      <option value=\"rgb(235, 214, 255)\" label=\"rgb(235, 214, 255)\"></option>\n      <option value=\"rgb(187, 187, 187)\" label=\"rgb(187, 187, 187)\"></option>\n      <option value=\"rgb(240, 102, 102)\" label=\"rgb(240, 102, 102)\"></option>\n      <option value=\"rgb(255, 194, 102)\" label=\"rgb(255, 194, 102)\"></option>\n      <option value=\"rgb(255, 255, 102)\" label=\"rgb(255, 255, 102)\"></option>\n      <option value=\"rgb(102, 185, 102)\" label=\"rgb(102, 185, 102)\"></option>\n      <option value=\"rgb(102, 163, 224)\" label=\"rgb(102, 163, 224)\"></option>\n      <option value=\"rgb(194, 133, 255)\" label=\"rgb(194, 133, 255)\"></option>\n      <option value=\"rgb(136, 136, 136)\" label=\"rgb(136, 136, 136)\"></option>\n      <option value=\"rgb(161, 0, 0)\" label=\"rgb(161, 0, 0)\"></option>\n      <option value=\"rgb(178, 107, 0)\" label=\"rgb(178, 107, 0)\"></option>\n      <option value=\"rgb(178, 178, 0)\" label=\"rgb(178, 178, 0)\"></option>\n      <option value=\"rgb(0, 97, 0)\" label=\"rgb(0, 97, 0)\"></option>\n      <option value=\"rgb(0, 71, 178)\" label=\"rgb(0, 71, 178)\"></option>\n      <option value=\"rgb(107, 36, 178)\" label=\"rgb(107, 36, 178)\"></option>\n      <option value=\"rgb(68, 68, 68)\" label=\"rgb(68, 68, 68)\"></option>\n      <option value=\"rgb(92, 0, 0)\" label=\"rgb(92, 0, 0)\"></option>\n      <option value=\"rgb(102, 61, 0)\" label=\"rgb(102, 61, 0)\"></option>\n      <option value=\"rgb(102, 102, 0)\" label=\"rgb(102, 102, 0)\"></option>\n      <option value=\"rgb(0, 55, 0)\" label=\"rgb(0, 55, 0)\"></option>\n      <option value=\"rgb(0, 41, 102)\" label=\"rgb(0, 41, 102)\"></option>\n      <option value=\"rgb(61, 20, 102)\" label=\"rgb(61, 20, 102)\"></option>\n    </select>\n  </span>\n  <span class=\"ql-format-group\">\n    <span title=\"List\" class=\"ql-format-button ql-list\"></span>\n    <span class=\"ql-format-separator\"></span>\n    <span title=\"Bullet\" class=\"ql-format-button ql-bullet\"></span>\n    <span class=\"ql-format-separator\"></span>\n    <select title=\"Text Alignment\" class=\"ql-align\">\n      <option value=\"left\" label=\"Left\" selected></option>\n      <option value=\"center\" label=\"Center\"></option>\n      <option value=\"right\" label=\"Right\"></option>\n      <option value=\"justify\" label=\"Justify\"></option>\n    </select>\n  </span>\n  <span class=\"ql-format-group\">\n    <span title=\"Link\" class=\"ql-format-button ql-link\"></span>\n    <span class=\"ql-format-separator\"></span>\n    <span title=\"Image\" class=\"ql-format-button ql-image\"></span>\n  </span>\n</div>";
       };
 
       return InlineEditor;
