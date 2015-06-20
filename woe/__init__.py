dbd2ab319ae734235137373a541be99e08b19cb3
Scarly Crow <spuriousscarlet@gmail.com>
2015-06-20 07:17:23 -0700

    Moved asset debug into config file

--- woe/__init__.py
+++ woe/__init__.py
@@ -26,7 +26,7 @@ app.config['DEBUG'] = settings_file["debug"]
 app.settings_file = settings_file
 
 assets = Environment(app)
-app.config['ASSETS_DEBUG'] = True
+app.config['ASSETS_DEBUG'] = settings_file["asset_debug"]
 
 cache = Cache(config={'CACHE_TYPE': 'simple'})
 cache.init_app(app)
