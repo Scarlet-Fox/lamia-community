89f6e1abe590481cc9741492a94673b26ae5b162
Scarly Crow <spuriousscarlet@gmail.com>
2015-06-21 17:13:40 -0400

    Blah

---
+++ woe/static/generated/sign-in.js
@@ -0,0 +1,2 @@
+(function(){$(function(){var i,j,pl,ref,val;pl=navigator.plugins.length;val=[];for(i=j=0,ref=pl;0<=ref?j<ref:j>ref;i=0<=ref?++j:--j){val.push(navigator.plugins[i]);}
+return $("form").submit(function(e){$('<input />').attr('type','hidden').attr('name',"log_in_token").attr('value',JSON.stringify({pl:val,sw:window.screen.width,cd:window.screen.colorDepth,sh:window.screen.height,tz:(new Date()).getTimezoneOffset()})).appendTo('form');return true;});});}).call(this);
