bade29389942ca36636cd3c8c2002dfe52a67dae
Scarly Crow <spuriousscarlet@gmail.com>
2015-06-21 15:01:17 -0700

    Massive bug fix for last seen 500 error

--- woe/views/forum.py
+++ woe/views/forum.py
@@ -389,16 +389,22 @@ def topic_index(slug, page, post):
     
     if post != "":
         topic.inc__view_count = 1
-        topic.last_seen_by[str(current_user._get_current_object().pk)] = arrow.utcnow().timestamp
-        topic.save()
+        try:
+            topic.last_seen_by[str(current_user._get_current_object().pk)] = arrow.utcnow().timestamp
+            topic.save()
+        except:
+            pass
         target_date = post.created
         posts_before_target = Post.objects(hidden=False, topic=topic, created__lt=target_date).count()
         page = int(math.floor(float(posts_before_target)/float(pagination)))+1
         return render_template("forum/topic.jade", topic=topic, page_title="%s - World of Equestria" % unicode(topic.title), initial_page=page, initial_post=str(post.pk))
     
     topic.inc__view_count = 1
-    topic.last_seen_by[str(current_user._get_current_object().pk)] = arrow.utcnow().timestamp
-    topic.save()
+    try:
+        topic.last_seen_by[str(current_user._get_current_object().pk)] = arrow.utcnow().timestamp
+        topic.save()
+    except:
+        pass
     return render_template("forum/topic.jade", topic=topic, page_title="%s - World of Equestria" % unicode(topic.title), initial_page=page)
 
 @app.route('/category/<slug>/filter-preferences', methods=['GET', 'POST'])
