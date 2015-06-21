089831d7d7f870970092b993e4042b419e911e11
Scarly Crow <spuriousscarlet@gmail.com>
2015-06-21 15:40:53 -0700

    Unicode error

--- woe/views/core.py
+++ woe/views/core.py
@@ -1094,7 +1094,7 @@ def sign_in():
             fingerprint_hash += unicode(key) + " "
             fingerprint_hash += unicode(value) + " "
             
-        _fingerprint_hash = hashlib.sha256(fingerprint_hash).hexdigest()
+        _fingerprint_hash = hashlib.sha256(unicode(fingerprint_hash)).hexdigest()
         
         try:
             f = Fingerprint.objects(fingerprint_hash=_fingerprint_hash)[0]
