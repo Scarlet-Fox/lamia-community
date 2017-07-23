d25b794bffa6aa790388e96f7ddeb6a62342d5c9
Scarly Crow <spuriousscarlet@gmail.com>
2017-07-22 21:28:26 -0700

    Made blogs prettier

--- woe/views/forum.py
+++ woe/views/forum.py
@@ -1449,7 +1449,7 @@ def index():
                 sqlm.Blog.privacy_setting == "all",
                 sqlm.Blog.privacy_setting == "members"
             )) \
-            .order_by(sqla.desc(sqlm.BlogEntry.published))[0:5]
+            .order_by(sqla.desc(sqlm.BlogEntry.published))[0:4]
     else:
         blogs = sqla.session.query(sqlm.Blog) \
             .join(sqlm.Blog.recent_entry) \
@@ -1459,7 +1459,7 @@ def index():
             .filter(sqla.or_(
                 sqlm.Blog.privacy_setting == "all"
             )) \
-            .order_by(sqla.desc(sqlm.BlogEntry.published))[0:5]
+            .order_by(sqla.desc(sqlm.BlogEntry.published))[0:4]
     
     tweets = sqla.session.query(sqlm.Tweet).order_by(sqla.desc(sqlm.Tweet.time))[:3]
     
