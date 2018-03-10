e9a8fde058f996c9ce135576c004a1b841800277
Scarly Crow <spuriousscarlet@gmail.com>
2018-03-10 11:59:08 -0800

    Thread import is completed

--- utilities/importscripts/Burning Board 4.x/1_forum_import.py
+++ utilities/importscripts/Burning Board 4.x/1_forum_import.py
@@ -123,3 +123,84 @@ for _cat in c.fetchall():
         continue
 
 c.close()
+
+# Add guest account
+new_user = User(
+    login_name = "_Guest Account_",
+    my_url = "_guest_account_",
+    display_name = "_Guest Account_",
+    email_address = "@"
+)
+new_user.set_password("_guest_account_")
+new_user.joined = arrow.utcnow().datetime
+new_user.over_thirteeen = False
+new_user.validated = False
+new_user.how_did_you_find_us = "Guest Account for Import"
+
+try:
+    sqla.session.add(new_user)
+    sqla.session.commit()
+except:
+    sqla.session.rollback()
+
+# Add topics
+
+c=db.cursor()
+c.execute(
+    """
+        SELECT t.*,
+        board.title AS board_name
+        FROM zuforum.wbb1_thread t
+        JOIN zuforum.wbb1_board board ON t.boardID = board.boardID
+    """
+)
+
+link_errors = 0
+
+guest_account = User.query.filter_by(my_url="_guest_account_").one()
+
+for t in c.fetchall():
+    new_topic = Topic(
+        title=t["topic"],
+        slug=find_topic_slug(t["topic"]),
+        sticky=bool(t["isSticky"]),
+        announcement=bool(t["isAnnouncement"]),
+        hidden=bool(t["isDeleted"]),
+        locked=bool(t["isDisabled"]) or bool(t["isClosed"]),
+        created=arrow.get(t["time"]).datetime
+    )
+    
+    category=Category.query.filter_by(
+        slug=slugify(t["board_name"], max_length=100, word_boundary=True, save_order=True)
+    ).all()
+    
+    if len(category) == 0:
+        __board_name = slugify(t["board_name"], max_length=100, word_boundary=True, save_order=True)
+        
+        if settings_file["import_db_topic_board_name_mapping"].has_key(__board_name):
+            category=Category.query.filter_by(
+                slug=settings_file["import_db_topic_board_name_mapping"][__board_name]
+            ).all()
+        else:
+            print "Topic category not found - topic #%s" % (t["threadID"],)
+        
+    new_topic.category = category[0]
+    
+    author=User.query.filter_by(
+        legacy_id=t["userID"]
+    ).all()
+    
+    if len(author) == 0:
+        new_topic.author = guest_account
+    else:
+        new_topic.author = author[0]
+    
+    try:
+        sqla.session.add(new_topic)
+        sqla.session.commit()
+    except IntegrityError as e:
+        sqla.session.rollback()
+        print e
+        continue
+
+c.close()
