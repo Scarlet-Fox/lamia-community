1d94181951b3c9c7b516e8f58ab9b24c0d75f46a
Scarly Crow <spuriousscarlet@gmail.com>
2018-04-18 19:02:17 -0700

    Added in some blog limitations #75

--- woe/forms/blogs.py
+++ woe/forms/blogs.py
@@ -6,7 +6,7 @@ import shutil, pytz, arrow
 
 class BlogSettingsForm(Form):
     title = StringField('Blog Name', [validators.InputRequired(), validators.Length(max=100)], default="")
-    description = HiddenField('Description', [validators.InputRequired()], default="")
+    description = HiddenField('Description', [validators.InputRequired(), validators.Length(max=2000)], default="")
     privacy_setting = SelectField('Privacy Setting', choices=[
             ("all", "Everyone"),
             ("members", "Only Members"),
@@ -16,9 +16,9 @@ class BlogSettingsForm(Form):
         ], default="members")
 
 class BlogEntryForm(Form):
-    title = StringField('Title', [validators.InputRequired(), validators.Length(max=100)], default="")
+    title = StringField('Title', [validators.InputRequired(), validators.Length(max=50000)], default="")
     entry = HiddenField('Post', [validators.InputRequired()], default="")
     draft = BooleanField('Draft / Mark as Hidden?', default=False)
 
 class BlogCommentForm(Form):
-    comment = HiddenField('Comment', [validators.InputRequired()], default="")
+    comment = HiddenField('Comment', [validators.InputRequired(), validators.Length(max=50000)], default="")
