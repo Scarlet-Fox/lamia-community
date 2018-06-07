62e43f733f7290a4d7da8caa26ca4ba9fb811ddb
ScarlyCrow <spuriousscarlet@gmail.com>
2018-06-06 21:14:50 -0700

    showing 5 blogs rather than 4

--- lamia/views/forum.py
+++ lamia/views/forum.py
@@ -1713,7 +1713,7 @@ def index():
                 sqlm.Blog.privacy_setting == "all",
                 sqlm.Blog.privacy_setting == "members"
             )) \
-            .order_by(sqla.desc(sqlm.BlogEntry.published))[0:4]
+            .order_by(sqla.desc(sqlm.BlogEntry.published))[0:5]
     else:
         blogs = sqla.session.query(sqlm.Blog) \
             .join(sqlm.Blog.recent_entry) \
@@ -1723,7 +1723,7 @@ def index():
             .filter(sqla.or_(
                 sqlm.Blog.privacy_setting == "all"
             )) \
-            .order_by(sqla.desc(sqlm.BlogEntry.published))[0:4]
+            .order_by(sqla.desc(sqlm.BlogEntry.published))[0:5]
     
     tweets = sqla.session.query(sqlm.Tweet).order_by(sqla.desc(sqlm.Tweet.time))[:3]
     
